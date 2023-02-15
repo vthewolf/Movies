0cb3effd5738b5362f1623dc48660b6b08381010
Víctor Santiago Ferrera <victor.santiago@the-cocktail.com>
2023-02-15 18:10:31 +0100

    ViewBinding added
    Creado un archivo txt con instrucciones sobre como utilizar viewbinding de forma simple

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.example.movies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        /*PARA EL VIEW BINDING HAY QUE DECLARAR ESTA VARIABLE
+        * El ActivityMainBinding es el fruto del nombre del xml en camelCase + Binding
+        * El binding representa todos los elementos que tenemos en el layout*/
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        /* Luego en el setContentView llamamos a binding.root. La raiz del componente
+        * que hemos inflado */
+        setContentView(binding.root)
+
+        // ACCEDER A VISTAS CON VIEWBINDING
+        binding.message.text = "Hello Android"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("Main Activity", "onDestroy")
     }
 }
